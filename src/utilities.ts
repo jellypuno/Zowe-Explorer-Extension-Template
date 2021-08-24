2c71810e9b88a3e6574bde63dbd40114a12d99ad
Jessielaine Punongbayan <jessielaine.punongbayan@broadcom.com>
2021-08-24 09:12:46 +0000

    added new APIs

--- src/utilities.ts
+++ src/utilities.ts
@@ -1,6 +1,5 @@
 import * as vscode from 'vscode';
-import { ZosmfSession } from '@zowe/cli';
-import { importedAPI } from './extension';
+import { IZoweDatasetTreeNode } from "@zowe/zowe-explorer-api";
 
 export async function setConfiguration () {
 
@@ -8,20 +7,6 @@ export async function setConfiguration () {
 
 }
 
-export async function getProfile (node: vscode.TreeItem) {
-    return importedAPI.getExplorerExtenderApi().getProfile(node);
-}
-
-export async function getSession (node: vscode.TreeItem) {
-    let session;
-    const getProfileInfo = await importedAPI.getExplorerExtenderApi().getProfile(node);
-
-	if (getProfileInfo) {
-		session = await ZosmfSession.createBasicZosmfSession(getProfileInfo.profile);
-		console.log('Creating Session for ', getProfileInfo.name);
-	} else {
-		vscode.window.showWarningMessage("Error in acquiring profile information");
-    }
-
-    return session;
+export async function getProfileInfo (node: IZoweDatasetTreeNode ) {
+    return node.getProfile();
 }
